cdf3deb53ec4cfff2477b3ad16e38d7978f084ce
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-20 11:54:49 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://blog-cqzj2oboq-vaibhavs-projects-26baaa7d.vercel.app",
       },
     },
   },
