d940a1fd982b998f020c28f12ba0398e1108e86c
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-21 12:19:43 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://blogapp-backend-6xke.onrender.com",
       },
     },
   },
