8f017e561c08e122ca7d06fbb7890a81725a649d
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-22 20:53:00 +0530

    update multer code

--- backend/src/middleware/multer.middlerware.js
+++ backend/src/middleware/multer.middlerware.js
@@ -3,10 +3,10 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + path.extname(file.originalname));
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   },
 });
 
