cb863b845c08c411c1fb8b8dfa73beb6b2535852
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-23 19:34:03 +0530

    add cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://my-blog-five-cyan.vercel.app'],
+    origin: ['https://my-blogs-virid-eight.vercel.app/'],
     credentials: true,
   })
 );
